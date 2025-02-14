1261cf85240029199340c586b5afca7899700e68
kambojhealth <billy@kambojhealth.com>
2025-02-14 16:14:57 +0500

    init

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import SiteHeader from "@/commonComponents/header/SiteHeader";
+// font-[family-name:var(--font-geist-sans)]
+export default function Home() {
+  return (
+    <div className="">
+      <SiteHeader theme={"White"} />
+    </div>
+  );
+}
